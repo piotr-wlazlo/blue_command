71324e9720222c66b2371d8971a348dc681da5be
Maciej Kidawa <251547@edu.p.lodz.pl>
2026-04-23 14:01:15 +0200

    Integrate Firebase Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("com.google.firebase:firebase-firestore")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +59,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
 }
--- app/src/main/java/com/project/blue_command/MainActivity.kt
+++ app/src/main/java/com/project/blue_command/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.project.blue_command
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.google.firebase.firestore.FirebaseFirestore
 import com.project.blue_command.presentation.AuthFlowScreen
 import com.project.blue_command.ui.theme.Blue_commandTheme
 
@@ -16,6 +18,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val db = FirebaseFirestore.getInstance()
+        val testCommand = hashMapOf(
+            "status" to "aktywne",
+            "wiadomosc" to "BlueCommand zgłasza gotowość!",
+            "dowodca" to "Maciek"
+        )
+
+        db.collection("test_polaczenia")
+            .add(testCommand)
+            .addOnSuccessListener { documentReference ->
+                Log.d("FIREBASE_TEST", "SUKCES! Dodano dokument o ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.e("FIREBASE_TEST", "BŁĄD! Nie udało się dodać dokumentu", e)
+            }
+
         setContent {
             Blue_commandTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
